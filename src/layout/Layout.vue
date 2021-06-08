320ba08d5fdc212efa37363cd204b669ba83b5a7
croquelois <croquelois.adrien@gmail.com>
2021-06-08 12:52:38 +0200

    fix: import component in router

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="layout-container">
     <img
-        id="logo"
-        :src="require('../assets/logo.png').default"
-        alt="logo"
+      id="logo"
+      :src="require('../assets/logo.png').default"
+      alt="logo"
+      @click="goToHome"
     />
   </div>
 </template>
 
 <script>
-export default {
-  name: "Layout",
-};
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "AlienAbduction",
+  setup() {
+    const router = useRouter();
+
+    function goToHome() {
+      router.push({ name: "Home"});
+    }
+
+    return {
+      goToHome,
+    };
+  },
+});
 </script>
 
 <style scoped>
@@ -20,9 +35,9 @@ export default {
   background-color: #f7eccc;
 }
 
-#logo{
+#logo {
   width: 80px;
   height: 80px;
-
+  cursor: pointer;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,10 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: () => import("../views/Home.vue"),
   },
   {
     path: "/games/alien-abduction",
